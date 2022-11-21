98e0a239eb00349756ce47a624bef45cb705f69f
messabih-khalil <msbih.khalil@gmail.com>
2022-11-21 21:25:33 +0100

    nuxt tut meta data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  app: {
+    head: {
+      title: "aldin store",
+      meta: [
+        {
+          name: "description",
+          content: "Aldin store today",
+        },
+      ],
+    },
+  },
 });
